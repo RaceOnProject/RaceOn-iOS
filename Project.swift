50afd54c28b6baf520cd65471130b65f17ca91e1
신승욱 <ukseung.dev@incross.com>
2024-11-22 14:03:36 +0900

    chore: tuist Proejct파일 수정
    close #1

--- Project.swift
+++ Project.swift
@@ -2,12 +2,35 @@ import ProjectDescription
 
 let project = Project(
     name: "RaceOn",
+    options: .options(
+        defaultKnownRegions: ["en", "ko"],
+        developmentRegion: "ko"
+    ),
+    packages: [
+        .remote(
+            url: "https://github.com/pointfreeco/swift-composable-architecture",
+            requirement: .upToNextMajor(from: "0.8.0")
+        ),
+        .remote(
+            url: "https://github.com/Moya/Moya.git",
+            requirement: .branch("master")
+        ),
+        .remote(
+            url: "https://github.com/onevcat/Kingfisher",
+            requirement: .branch("master")
+        ),
+        .remote(
+            url: "https://github.com/no-comment/KeyboardToolbar.git",
+            requirement: .branch("main")
+        )
+    ],
     targets: [
         .target(
             name: "RaceOn",
             destinations: .iOS,
             product: .app,
             bundleId: "io.tuist.RaceOn",
+            deploymentTargets: .iOS("16.0"),
             infoPlist: .extendingDefault(
                 with: [
                     "UILaunchScreen": [
@@ -18,7 +41,20 @@ let project = Project(
             ),
             sources: ["RaceOn/Sources/**"],
             resources: ["RaceOn/Resources/**"],
-            dependencies: []
+            dependencies: [
+                .package(product: "ComposableArchitecture"),
+                .package(product: "Moya"),
+                .package(product: "Kingfisher"),
+                .package(product: "KeyboardToolbar")
+            ],
+            settings: .settings(
+                base: [:],
+                configurations: [
+                    .debug(name: "Debug"),
+                    .debug(name: "QA"),
+                    .release(name: "Release"),
+                ]
+            )
         ),
         .target(
             name: "RaceOnTests",
@@ -28,7 +64,13 @@ let project = Project(
             infoPlist: .default,
             sources: ["RaceOn/Tests/**"],
             resources: [],
-            dependencies: [.target(name: "RaceOn")]
+            dependencies: [
+                .target(name: "RaceOn"),
+                .package(product: "ComposableArchitecture"),
+                .package(product: "Moya"),
+                .package(product: "Kingfisher"),
+                .package(product: "KeyboardToolbar")
+            ]
         ),
     ]
 )
