7f02f8ae259bb906bfd0ba2f8ac7550fadb22862
shinseunguk <krdut1@gmail.com>
2024-12-15 16:40:27 +0900

    fix: Navigation Swipe가 안되던 현상 해결
    close #17

---
+++ RaceOn/Shared/Sources/Extension/UIViewController+Extension.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewController+Extension.swift
+//  Shared
+//
+//  Created by ukBook on 12/15/24.
+//
+
+import UIKit
+
+extension UINavigationController: UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        return viewControllers.count > 1
+    }
+}
