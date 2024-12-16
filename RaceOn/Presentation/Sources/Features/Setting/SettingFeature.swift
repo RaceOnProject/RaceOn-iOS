5a02427b021e22d58634d1d677cb0a852dd1a07e
신승욱 <ukseung.dev@incross.com>
2024-12-16 15:45:32 +0900

    fix: 첫 진입시에 버전이 nil로 오는 이슈 해결

--- RaceOn/Presentation/Sources/Features/Setting/SettingFeature.swift
+++ RaceOn/Presentation/Sources/Features/Setting/SettingFeature.swift
@@ -14,7 +14,7 @@ public struct SettingFeature {
     public init() {}
     
     public struct State: Equatable {
-        var currentVersion: String?
+        var currentVersion: String? = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String
         
         public init() {}
     }
@@ -27,19 +27,9 @@ public struct SettingFeature {
     public func reduce(into state: inout State, action: Action) -> Effect<Action> {
         switch action {
         case .onAppear:
-            guard let currentVersion = getAppVersion() else { return .none }
-            print("currentVersion => \(currentVersion)")
-            state.currentVersion = currentVersion
             return .none
         case .noAction:
             return .none
         }
-    }
-    
-    func getAppVersion() -> String? {
-        guard let version = Bundle.main.infoDictionary?["CFBundleShortVersionString"] as? String else {
-            return nil
-        }
-        return version
     }
 }
