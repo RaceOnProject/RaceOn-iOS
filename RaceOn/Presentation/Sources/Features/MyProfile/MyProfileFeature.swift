9ec8f9ad0edc16d0fb46422e20725e7970384c9f
신승욱 <ukseung.dev@incross.com>
2024-12-17 15:29:42 +0900

    feat: 내 프로필 UI 작업
    close #20

--- RaceOn/Presentation/Sources/Features/MyProfile/MyProfileFeature.swift
+++ RaceOn/Presentation/Sources/Features/MyProfile/MyProfileFeature.swift
@@ -11,20 +11,30 @@ import SwiftUI
 @Reducer
 struct MyProfileFeature {
     struct State: Equatable {
+        var nickname: String = ""
         var friendCode: String = "GD231E"
+        var isEditing: Bool = false
         var toast: Toast?
     }
     
     enum Action {
+        case onAppear
         case copyButtonTapped
+        case navigationTrailingButtonTapped(isEditing: Bool)
         case dismissToast
     }
     
     func reduce(into state: inout State, action: Action) -> Effect<Action> {
         switch action {
+        case .onAppear:
+            state.nickname = "random name"
+            return .none
         case .copyButtonTapped:
             UIPasteboard.general.string = state.friendCode
             state.toast = Toast(content: "내 코드가 복사되었어요")
+            return .none
+        case .navigationTrailingButtonTapped(let isEditing):
+            state.isEditing = isEditing
             return .none
         case .dismissToast:
             state.toast = nil
