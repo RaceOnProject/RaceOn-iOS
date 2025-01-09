414eb3d776cb2585faf90c70020c42833b8346b7
정진우 <wlsdn4808@naver.com>
2025-01-09 18:15:20 +0900

    feat: 카카오, 애플로그인 추가, id토큰 제공

--- RaceOn/Presentation/Sources/Features/Login/LoginFeature.swift
+++ RaceOn/Presentation/Sources/Features/Login/LoginFeature.swift
@@ -8,13 +8,20 @@
 import SwiftUI
 import Foundation
 import CoreLocation
+import AuthenticationServices
 
 import ComposableArchitecture
 import KakaoSDKUser
 import KakaoSDKAuth
 
+public enum SocialLoginType: String {
+    case kakao = "KAKAO"
+    case apple = "APPLE"
+}
+
 @Reducer
 public struct LoginFeature {
+    
     public init () {}
     
     public struct State: Equatable {
@@ -23,56 +30,75 @@ public struct LoginFeature {
     
     public enum Action: Equatable {
         case kakaoLoginButtonTapped
+        case requestLogin(String, SocialLoginType)
     }
     
     public func reduce(into state: inout State, action: Action) -> Effect<Action> {
         switch action {
         case .kakaoLoginButtonTapped:
-            
-            return .none
+            if !UserApi.isKakaoTalkLoginAvailable() {
+                // 카카오톡 앱으로 로그인
+                return kakaoLoginWithApp()
+            } else {
+                // 카톡이 설치가 안 되어 있을 때
+                // 카카오 계정으로 로그인
+                return kakaoLoginWithAccount()
+            }
+        case .requestLogin(let idToken, let socialLoginType):
+            return requestLogin(idToken: idToken, socialLoginType: socialLoginType)
         }
     }
 }
 // MARK: - KAKAO LOGIN
 public extension LoginFeature {
-    private func kakaoLogin() {
-        // 카카오톡 실행 가능 여부 확인
-        if (UserApi.isKakaoTalkLoginAvailable()) {
-            // 카카오톡 앱으로 로그인 인증
-            kakaoLonginWithApp()
-        } else { // 카톡이 설치가 안 되어 있을 때
-            // 카카오 계정으로 로그인
-            kakaoLoginWithAccount()
+    
+    private func kakaoLoginWithApp() -> Effect<Action> {
+        return .run { @MainActor send in
+            await withCheckedContinuation { continuation in
+                UserApi.shared.loginWithKakaoTalk { (oauthToken, error) in
+                    if let error = error {
+                        print("카카오톡 앱 로그인 실패: \(error.localizedDescription)")
+                        continuation.resume()
+                    } else if let oauthToken = oauthToken {
+                        guard let idToken = oauthToken.idToken else {
+                            continuation.resume()
+                            return
+                        }
+                        
+                        send(.requestLogin(idToken, .kakao))
+                        continuation.resume()
+                    }
+                }
+            }
         }
     }
     
-    func kakaoLonginWithApp() {
-        UserApi.shared.loginWithKakaoTalk {(oauthToken, error) in
-            if let error = error {
-                print(error)
-            }
-            else {
-                print("loginWithKakaoTalk() success.")
-                
-                // TODO: API 호출
-                _ = oauthToken
+    private func kakaoLoginWithAccount() -> Effect<Action> {
+        return .run { @MainActor send in
+            await withCheckedContinuation { continuation in
+                UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
+                    if let error = error {
+                        print("카카오톡 계정 로그인 실패: \(error.localizedDescription)")
+                        continuation.resume()
+                    } else {
+                        guard let idToken = oauthToken?.idToken else {
+                            continuation.resume()
+                            return
+                        }
+                        
+                        send(.requestLogin(idToken, .kakao))
+                        continuation.resume()
+                    }
+                }
             }
         }
     }
-    
-    
-    private func kakaoLoginWithAccount() {
-        
-        UserApi.shared.loginWithKakaoAccount {(oauthToken, error) in
-            if let error = error {
-                print(error)
-            }
-            else {
-                print("loginWithKakaoAccount() success.")
-                
-                // TODO: API 호출
-                _ = oauthToken
-            }
-        }
+}
+
+// MARK: - LOGIN API
+public extension LoginFeature {
+    private func requestLogin(idToken: String, socialLoginType: SocialLoginType) -> Effect<Action> {
+        print("idTokens: \(idToken), socialType: \(socialLoginType)")
+        return .none
     }
 }
