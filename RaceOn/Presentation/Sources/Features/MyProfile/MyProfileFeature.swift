d7b6440fab9b8857c2f4a14983f15587dc235851
shinseunguk <krdut1@gmail.com>
2025-01-15 21:46:16 +0900

    refactor: 코드리뷰 대응

--- RaceOn/Presentation/Sources/Features/MyProfile/MyProfileFeature.swift
+++ RaceOn/Presentation/Sources/Features/MyProfile/MyProfileFeature.swift
@@ -52,6 +52,8 @@ struct MyProfileFeature {
         case setErrorMessage(String)
         
         case editNickname(String)
+        
+        case logout
     }
     
     @Dependency(\.memberUseCase) var memberUseCase
@@ -60,26 +62,10 @@ struct MyProfileFeature {
         switch action {
         case .onAppear:
             state.isLoading = true
-//            guard let memberId: Int = UserDefaultsManager.shared.get(forKey: .memberId) else { return .none }
-            // FIXME: TEST용 (임시 로그인)
-            let memberId: Int = 1
-            
-            return Effect.publisher {
-                memberUseCase.fetchMemberInfo(memberId: memberId)
-                    .receive(on: DispatchQueue.main)
-                    .map {
-                        if let memberInfo = $0.data {
-                            return Action.setMemberInfo(memberInfo)
-                        } else {
-                            return Action.setErrorMessage("멤버 정보를 찾을 수 없습니다.")
-                        }
-                    }
-                    .catch { error -> Just<Action> in
-                        let errorMessage = error.message
-                        return Just(Action.setErrorMessage(errorMessage))
-                    }
-                    .eraseToAnyPublisher()
+            guard let memberId: Int = UserDefaultsManager.shared.get(forKey: .memberId) else {
+                return .send(.logout)
             }
+            return fetchMemberInfo(memberId: memberId)
         case .copyButtonTapped:
             guard let memberCode = state.memberCode else { return .none }
             UIPasteboard.general.string = memberCode
@@ -90,88 +76,17 @@ struct MyProfileFeature {
             return .none
         case .saveButtonTapped:
             state.isLoading = true
-            
-            // FIXME: TEST용 (임시 로그인)
-            let memberId: Int = 1
-            return Effect.publisher {
-                memberUseCase.requestImageEditPermission(memberId: memberId)
-                    .receive(on: DispatchQueue.main)
-                    .map {
-                        Action.uploadImageS3($0)
-                    }
-                    .catch { error in
-                        return Just(Action.setErrorMessage(error.message))
-                    }
+            guard let memberId: Int = UserDefaultsManager.shared.get(forKey: .memberId) else {
+                return .send(.logout)
             }
+            return requestImageEditPermission(memberId: memberId)
         case .uploadImageS3(let response):
-            if let selectedImage = state.selectedImage,
-               let contentUrl = response.data?.contentUrl,
-               let imageData = resizeImage(
-                image: selectedImage,
-                targetSize: CGSize(width: 128, height: 128)
-               ).pngData() {
-                state.profileImageUrl = contentUrl
-                
-                return Effect.publisher {
-                    memberUseCase.uploadImageS3(
-                        url: response.data?.preSignedUrl ?? .init(),
-                        pngData: imageData
-                    )
-                    .receive(on: DispatchQueue.main)
-                        .map { _ in
-                            Action.editProfile
-                        }
-                        .catch { error in
-                            Just(Action.setErrorMessage(error.message))
-                        }
-                }
-            } else {
-                guard let nickname = state.nickname,
-                      let contentUrl = state.profileImageUrl else {
-                    return .none
-                }
-                
-                // FIXME: TEST용 (임시 로그인)
-                let memberId: Int = 1
-                
-                return Effect.publisher {
-                    memberUseCase.updateProfile(
-                        memberId: memberId,
-                        contentUrl: contentUrl,
-                        nickname: nickname
-                    )
-                        .receive(on: DispatchQueue.main)
-                        .map { _ in
-                            Action.editProfileResponse
-                        }
-                        .catch { error in
-                            Just(Action.setErrorMessage(error.message))
-                        }
-                }
-            }
+            return handleUploadImageS3(response: response, state: &state)
         case .editProfile:
-            guard let nickname = state.nickname,
-                  let contentUrl = state.profileImageUrl else {
-                return .none
+            guard let memberId: Int = UserDefaultsManager.shared.get(forKey: .memberId) else {
+                return .send(.logout)
             }
-            
-            // FIXME: TEST용 (임시 로그인)
-            let memberId: Int = 1
-            
-            return Effect.publisher {
-                memberUseCase.updateProfile(
-                    memberId: memberId,
-                    contentUrl: contentUrl,
-                    nickname: nickname
-                )
-                    .receive(on: DispatchQueue.main)
-                    .map { _ in
-                        Action.editProfileResponse
-                    }
-                    .catch { error in
-                        Just(Action.setErrorMessage(error.message))
-                    }
-            }
+            return updateProfile(memberId: memberId, state: &state)
         case .dismissToast:
             state.toast = nil
             return .none
@@ -202,31 +117,104 @@ struct MyProfileFeature {
         case .editNickname(let nickname):
             state.nickname = nickname
             return .none
+        case .logout:
+            // TODO: Logout
+            return .none
         }
     }
-}
-
-/// UIImage를 지정된 크기로 리사이징하는 함수
-func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
-    let size = image.size
-    let widthRatio  = targetSize.width  / size.width
-    let heightRatio = targetSize.height / size.height
     
-    // 크기를 유지하면서 리사이징 비율을 계산합니다
-    let newSize: CGSize
-    if widthRatio > heightRatio {
-        newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
-    } else {
-        newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
+    private func fetchMemberInfo(memberId: Int) -> Effect<Action> {
+        return Effect.publisher {
+            memberUseCase.fetchMemberInfo(memberId: memberId)
+                .receive(on: DispatchQueue.main)
+                .map {
+                    if let memberInfo = $0.data {
+                        return Action.setMemberInfo(memberInfo)
+                    } else {
+                        return Action.setErrorMessage("멤버 정보를 찾을 수 없습니다.")
+                    }
+                }
+                .catch { error -> Just<Action> in
+                    let errorMessage = error.message
+                    return Just(Action.setErrorMessage(errorMessage))
+                }
+                .eraseToAnyPublisher()
+        }
     }
     
-    // 리사이징 후 새 이미지를 생성합니다
-    let rect = CGRect(origin: .zero, size: newSize)
-    UIGraphicsBeginImageContextWithOptions(newSize, false, image.scale)
-    image.draw(in: rect)
-    let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
-    UIGraphicsEndImageContext()
+    private func requestImageEditPermission(memberId: Int) -> Effect<Action> {
+        return Effect.publisher {
+            memberUseCase.requestImageEditPermission(memberId: memberId)
+                .receive(on: DispatchQueue.main)
+                .map { Action.uploadImageS3($0) }
+                .catch { error in Just(Action.setErrorMessage(error.message)) }
+        }
+    }
     
-    return resizedImage ?? image
+    private func handleUploadImageS3(response: BaseResponse<ProfileImageResponse>, state: inout State) -> Effect<Action> {
+        if let selectedImage = state.selectedImage,
+           let contentUrl = response.data?.contentUrl,
+           let imageData = resizeImage(
+            image: selectedImage,
+            targetSize: CGSize(width: 128, height: 128)
+           ).pngData() {
+            state.profileImageUrl = contentUrl
+            
+            return Effect.publisher {
+                memberUseCase.uploadImageS3(
+                    url: response.data?.preSignedUrl ?? .init(),
+                    pngData: imageData
+                )
+                .receive(on: DispatchQueue.main)
+                .map { _ in Action.editProfile }
+                .catch { error in Just(Action.setErrorMessage(error.message)) }
+            }
+        } else {
+            guard let memberId: Int = UserDefaultsManager.shared.get(forKey: .memberId) else {
+                return .send(.logout)
+            }
+            return updateProfile(memberId: memberId, state: &state)
+        }
+    }
+    
+    private func updateProfile(memberId: Int, state: inout State) -> Effect<Action> {
+        guard let nickname = state.nickname, let contentUrl = state.profileImageUrl else {
+            return .none
+        }
+        
+        return Effect.publisher {
+            memberUseCase.updateProfile(
+                memberId: memberId,
+                contentUrl: contentUrl,
+                nickname: nickname
+            )
+            .receive(on: DispatchQueue.main)
+            .map { _ in Action.editProfileResponse }
+            .catch { error in Just(Action.setErrorMessage(error.message)) }
+        }
+    }
+    
+    /// UIImage를 지정된 크기로 리사이징하는 함수
+    private func resizeImage(image: UIImage, targetSize: CGSize) -> UIImage {
+        let size = image.size
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+        
+        // 크기를 유지하면서 리사이징 비율을 계산합니다
+        let newSize: CGSize
+        if widthRatio > heightRatio {
+            newSize = CGSize(width: size.width * heightRatio, height: size.height * heightRatio)
+        } else {
+            newSize = CGSize(width: size.width * widthRatio, height: size.height * widthRatio)
+        }
+        
+        // 리사이징 후 새 이미지를 생성합니다
+        let rect = CGRect(origin: .zero, size: newSize)
+        UIGraphicsBeginImageContextWithOptions(newSize, false, image.scale)
+        image.draw(in: rect)
+        let resizedImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        
+        return resizedImage ?? image
+    }
 }
-
