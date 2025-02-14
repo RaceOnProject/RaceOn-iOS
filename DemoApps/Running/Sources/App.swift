7d3ee5265bd1710a753186a4b73e9adcfff56476
신승욱 <ukseung.dev@incross.com>
2025-02-14 13:21:54 +0900

    fix: 네비게이션 공간이 보이던 현상 제거

--- DemoApps/Running/Sources/App.swift
+++ DemoApps/Running/Sources/App.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import Presentation
 import ComposableArchitecture
 import Domain
+import NMapsMap
 
 @main
 struct MyApp: App {
@@ -19,7 +20,7 @@ struct MyApp: App {
             NavigationStack(path: $router.route) {
                 VStack {
                     Button {
-//                        router.push(screen: .matchingProcess(.three, Friend()))
+//                        router.push(screen: .matchingProcess(<#T##MatchingDistance#>, friendId: <#T##Int#>, isInvited: <#T##Bool#>, gameId: <#T##Int?#>))
                     } label: {
                         Text("대기 화면으로")
                             .foregroundColor(.white)
@@ -38,7 +39,22 @@ struct MyApp: App {
                     
                     Button {
                         print("경쟁 완료 화면으로")
-//                        router.push(screen: .finishGame)
+                        router.push(screen:
+                                .finishGame(
+                                    myProfileURL: "https://k.kakaocdn.net/dn/bf8Afk/btsDfp2vKkG/hu9Yrq95AyLMm1K9DCFqiK/img_640x640.jpg",
+                                    opponentURL: "https://race-on.s3.ap-northeast-2.amazonaws.com/profileimage/basic_profile.png",
+                                    opponentNickname: "조용한여우1234",
+                                    myTotalDistance: 3.00,
+                                    opponentTotalDistance: 2.55,
+                                    averagePace: "4′35″",
+                                    userLocationArray: [
+                                        NMGLatLng(lat: 37.55474, lng: 126.9704),
+                                        NMGLatLng(lat: 37.5547421, lng: 126.9704338),
+                                        NMGLatLng(lat: 37.55450, lng: 126.9666),
+                                        NMGLatLng(lat: 37.55290, lng: 126.9643)
+                                    ]
+                                )
+                        )
                     } label: {
                         Text(("경쟁 완료 화면으로"))
                             .foregroundColor(.white)
