e0cb8d24dbb98cf89d80bbb2c83b25e14a20ff83
신승욱 <ukseung.dev@incross.com>
2025-02-17 13:16:57 +0900

    feat: 게임 종료 조건 수정 및 웹훅으로 State property 변경

--- RaceOn/Domain/Sources/Entities/Game/GameResponse.swift
+++ RaceOn/Domain/Sources/Entities/Game/GameResponse.swift
@@ -6,3 +6,23 @@
 //
 
 import Foundation
+
+public struct ResponseData: Decodable {
+    public let command: String
+    public let statusCode: Int
+    public let code: String
+    public let message: String
+    public let data: GameResponse
+    public let success: Bool
+}
+
+public struct GameResponse: Decodable {
+    public let gameId: Int
+    public let memberId: Int
+    public let time: String
+    public let latitude: Double
+    public let longitude: Double
+    public let distance: Double
+    public let winMemberId: Int?
+    public let finished: Bool
+}
